cba9aa466595cdf839e16a845dcf1f49dc2feb51
Danial Othman <danialbinothman@gmail.com>
2022-03-14 23:44:38 +0800

    added staticWhite style

--- simple_neopixel_by_dan.ino
+++ simple_neopixel_by_dan.ino
@@ -2,6 +2,7 @@
 
 #define PIN       6
 #define NUMPIXELS 30 // number of LED on strip
+#define BRIGHTNESS 155 // 0 - 255 dim to bright
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800); // essential for lib
 
@@ -9,7 +10,7 @@ Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800); // essential for
 
 void setup() {
   Serial.begin(9600);
-  pixels.setBrightness(50);
+  pixels.setBrightness(BRIGHTNESS);
   pixels.begin();
 
 }
@@ -17,15 +18,27 @@ void setup() {
 void loop() {
   pixels.clear();
 
-  //test style
+  //test style - comment or uncomment to try
   kitRun();
+  //  staticWhite();
+}
+
+
+//static
+void staticWhite() {
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(255, 255, 255));
+  }
+  pixels.show();
+  delay(DELAYVAL);
+
 }
 
 //knightRider style
 void kitRun() {
   for (int i = 0; i < NUMPIXELS; i++) {
     Serial.println(i);
-    pixels.setPixelColor(i, pixels.Color(0, 255, 0));
+    pixels.setPixelColor(i, pixels.Color(0, 150, 150));
     if (i != 0) {
       pixels.setPixelColor(i - 1, pixels.Color(0, 0, 0));
     }
@@ -36,7 +49,7 @@ void kitRun() {
       pixels.clear();
       for (int j = NUMPIXELS - 2; j > 0; j--) {
         Serial.println(j);
-        pixels.setPixelColor(j, pixels.Color(0, 255, 0));
+        pixels.setPixelColor(j, pixels.Color(0, 150, 150));
         if (j != NUMPIXELS) {
           pixels.setPixelColor(j + 1, pixels.Color(0, 0, 0));
         }
