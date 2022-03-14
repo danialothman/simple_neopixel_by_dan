0de4a073117c860f62e9d0289f6940547c60f629
Danial Othman <danialbinothman@gmail.com>
2022-03-14 21:15:25 +0800

    Update simple_neopixel_by_dan.ino
    some comments, added knightRider style function.

--- simple_neopixel_by_dan.ino
+++ simple_neopixel_by_dan.ino
@@ -1,9 +1,9 @@
 #include <Adafruit_NeoPixel.h>
 
 #define PIN       6
-#define NUMPIXELS 30
+#define NUMPIXELS 30 // number of LED on strip
 
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800); // essential for lib
 
 #define DELAYVAL 250
 
@@ -16,28 +16,33 @@ void setup() {
 
 void loop() {
   pixels.clear();
-  
+
+  //test style
+  kitRun();
+}
+
+//knightRider style
+void kitRun() {
   for (int i = 0; i < NUMPIXELS; i++) {
     Serial.println(i);
     pixels.setPixelColor(i, pixels.Color(0, 255, 0));
     if (i != 0) {
-      pixels.setPixelColor(i-1, pixels.Color(0, 0, 0));
+      pixels.setPixelColor(i - 1, pixels.Color(0, 0, 0));
     }
     pixels.show();
     delay(DELAYVAL);
-    if (i == NUMPIXELS-1) {
+    if (i == NUMPIXELS - 1) {
       Serial.println("SWITCH");
       pixels.clear();
-      for (int j = NUMPIXELS-2; j > 0; j--) {
+      for (int j = NUMPIXELS - 2; j > 0; j--) {
         Serial.println(j);
         pixels.setPixelColor(j, pixels.Color(0, 255, 0));
         if (j != NUMPIXELS) {
-          pixels.setPixelColor(j+1, pixels.Color(0, 0, 0));
+          pixels.setPixelColor(j + 1, pixels.Color(0, 0, 0));
         }
         pixels.show();
         delay(DELAYVAL);
-      }     
+      }
     }
   }
-
 }
